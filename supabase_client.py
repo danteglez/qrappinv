3191a6935a958b0c02cdfbd673e4a17a57122b97
danteglez <129199887+danteglez@users.noreply.github.com>
2025-05-27 18:47:23 -0600

    Add files via upload

--- supabase_client.py
+++ supabase_client.py
@@ -1,6 +1,6 @@
 from supabase import create_client
 
 url = "https://avxyefrckoynbubddwhl.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."  # recortado por seguridad
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2eHllZnJja295bmJ1YmRkd2hsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0ODM4ODQ3MCwiZXhwIjoyMDYzOTY0NDcwfQ.WC0RvJyNlGM_yxXzmCo4BHBtxUiJMkesg1TbLyOCp_k"  # recortado por seguridad
 
 supabase = create_client(url, key)
